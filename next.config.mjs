c603f3735cd1d77a669d1d4808babf7bd4bcae7b
KishoreKode <kishoreganth700@gmail.com>
2025-01-25 22:29:08 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     env:{
 
          url:'https://4b63-106-219-183-231.ngrok-free.app',
-         chat_url:'https://chat.zoft.ai',
+         chat_url:' https://b31a-106-219-183-231.ngrok-free.app ',
          boturl:'https://kayzen-backend.onrender.com'
     }
 };
