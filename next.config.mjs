33e01677355395d6f59f3ffbb01d05cfb4a76214
kishoreganth <kishoreganth700@gmail.com>
2025-02-06 14:49:31 +0530

    latest

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-
          url:'https://api.zoft.ai',
          chat_url:'https://chat.zoft.ai',
          boturl:'https://chat-embed.zoft.ai/',
