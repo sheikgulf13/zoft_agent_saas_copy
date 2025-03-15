7cb3e3225310e264a78d5507017f704f5ec75599
unknown <=>
2025-03-15 21:22:28 +0530

    fix: calendly integration

--- app/integrations/page.jsx
+++ app/integrations/page.jsx
@@ -1,14 +1,44 @@
-"use client"
+"use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  getIntegrationList,
+  removeIntegrationApi,
+} from "../../api/integration-base-api";
+import { ContainedButton } from "@/Components/buttons/ContainedButton";
 
 const Page = () => {
+  const [integrations, setIntegrations] = useState({});
+
   const authenticateCalendly = () => {
     const url =
-      "https://auth.calendly.com/oauth/authorize?client_id=b-q2YaJTFOgGRf56tlWs0j1bY1PYRELbKWheaT48rG4&response_type=code&redirect_uri=https://app.zoft.ai/dashboard";
+      "https://auth.calendly.com/oauth/authorize?client_id=jq3Jg3umJnGpDjSjfRtdrvt3SZE9yVxhiVdcBNgtbiI&response_type=code&redirect_uri=http://localhost:3001/dashboard";
 
     window.open(url);
   };
+
+  const loadAndStoreIntegrationList = async () => {
+    const integrations = await getIntegrationList();
+
+    setIntegrations(integrations ? integrations : {});
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      loadAndStoreIntegrationList();
+    }, 2000);
+
+    // Cleanup function to clear the interval when component unmounts
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array means this effect runs once on mount
+
+  const removeIntegration = (event) => {
+    event.stopPropagation();
+    removeIntegrationApi("calendly");
+  };
+
+  console.log(integrations);
+
   return (
     <div>
       <div className="w-full h-screen relative flex justify-center items-center">
@@ -16,7 +46,7 @@ const Page = () => {
           <h3 className="text-xl font-bold mb-8">Integrations</h3>
           <div className="flex">
             <div
-              className="w-[290px] h-[170px] bg-white rounded-[8px] border-[#F2F2F2]-1 p-2 cursor-pointer"
+              className="w-[290px] bg-white rounded-[8px] border-[#F2F2F2]-1 p-4 cursor-pointer flex flex-col gap-2"
               onClick={authenticateCalendly}
             >
               <div className="w-full h-[40%]">
@@ -28,6 +58,17 @@ const Page = () => {
                   Integrate with calendly to automate bookings via chatbot and
                   phonebot
                 </p>
+                {integrations["calendly"] && (
+                  <>
+                    <div className="text-sm text-green-500">Connected</div>
+                    <ContainedButton
+                      backgroundColor={"rgb(239 68 68 / var(--tw-bg-opacity))"}
+                      onClick={removeIntegration}
+                    >
+                      Disconnect
+                    </ContainedButton>
+                  </>
+                )}
               </div>
             </div>
           </div>
