dc705a4cabf69aa08da3ab442c09cf9c67e231a7
kishoreganth <kishoreganth700@gmail.com>
2025-03-28 13:34:25 +0530

    updated api url

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-         url:'https://3c53-49-206-11-27.ngrok-free.app',
+         url:'https://api.zoft.ai',
          chat_url:'https://chat.zoft.ai',
          boturl:'https://chat-embed.zoft.ai/',
          phone_url:'https://phone.zoft.ai',
