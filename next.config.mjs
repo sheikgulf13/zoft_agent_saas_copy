fc28f594db987a4c9678b29abf0a2ae43bcf2391
kishoreganth <kishoreganth700@gmail.com>
2025-06-09 14:02:06 +0530

    updated working UI for chat actions update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-         url:'https://6ab3-106-219-176-153.ngrok-free.app',
+         url:'https://api.zoft.ai',
          //url:'https://3f15-106-219-176-153.ngrok-free.app',
          chat_url:'https://chat.zoft.ai',
          //chat_url:'https://aa9a-106-219-179-151.ngrok-free.app',
